49b14e1390ec06f960e68f74891dc76cbb1e3ed1
Alex Teixeira <alextsilva08@gmail.com>
2025-05-20 23:20:05 -0300

    Finalização da página inicial com animações de transição

--- Front_end/command.js
+++ Front_end/command.js
@@ -1,7 +1,21 @@
-document.addEventListener("DOMContentLoaded",function({
-    
+document.addEventListener('DOMContentLoaded', function () {
+  const cadastrar = document.getElementById("cadastrar");
+  const entrar = document.getElementById("entrar");
+  const container = document.querySelector(".container");
+  const esqueceuSenha = document.querySelectorAll(".esqueceuSenha");
 
+    cadastrar.addEventListener("click", () => {
+        container.classList.add("painel-direito-ativo");
+    });
 
+    esqueceuSenha.forEach(link => {
+        link.addEventListener("click", () => {
+            container.classList.add("painel-direito-ativo");
+        })
+    })
 
+    entrar.addEventListener("click", () => {
+        container.classList.remove("painel-direito-ativo");
+    });
 
-}));
+});
